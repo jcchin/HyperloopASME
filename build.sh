3d16ba4815a7b9ed83902ba54dee78dcc29e4991
Jeff Chin <jchin89@gmail.com>
2015-07-06 14:34:11 -0400

    fixed bib, added required packages

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 #build pdf (with bibtex), by defualt do a quick build
 "/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
 if [ "$1" == "-f" ]; then #add -f flag to do full build
-	bibtex heading.aux
+	bibtex asme2e
 	makeindex heading.nlo  -s nomencl.ist -o heading.nls
 	"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
 	"/usr/texbin/pdflatex" -synctex=1 -shell-escape -interaction=nonstopmode heading.tex
 
-	mv heading.pdf hyperloop.pdf
 fi
 #. clean.sh
